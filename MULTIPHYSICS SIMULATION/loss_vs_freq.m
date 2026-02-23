51a930f837fd9c57b9fe3291734cf8104d31c456
mahmud10cool <mutashi@gmail.com>
2026-02-23 15:00:15 -0600

    this is the new design

--- MULTIPHYSICS SIMULATION/loss_vs_freq.m
+++ MULTIPHYSICS SIMULATION/loss_vs_freq.m
@@ -9,11 +9,11 @@ param.zeta = 1;
 wn_size = 15;
 wn = logspace(3,6,wn_size);
 
-xdot_size = 1;
-xdot = linspace(0,10e-2,xdot_size);
+xdot_size = 7;
+xdot = linspace(0,6e-2,xdot_size);
 
-open_ratio_size = 1;
-open_ratio_array = linspace(2,25,open_ratio_size);
+open_ratio_size = 6;
+open_ratio_array = logspace(0,5,open_ratio_size);
 
 Loss = NaN(wn_size,xdot_size,open_ratio_size);
 Vx_final = NaN(wn_size,xdot_size,open_ratio_size);
@@ -38,33 +38,67 @@ end
 switching_time = 1./(wn*2);
 
 %% Plots
-Loss2 = squeeze(Loss(:,1,:));
-[OR, Tsw] = meshgrid(open_ratio_array, switching_time*1e3);
+[WN,XDOT,OR] = meshgrid(wn,xdot,open_ratio_array);
 
-figure;
-surf(OR, Tsw, Loss2);
-xlabel('Opening ratio');
-ylabel('Switching time (s)');
-zlabel('Loss');
-set(gca,'YScale','log'); % switching_time is also log-spaced here
-% set(gca,'Ydir', 'reverse')
-grid on;
+X = log10(WN(:));     % log because wn is logspace
+Y = XDOT(:);
+Z = Loss(:);
+
+K = convhull(X,Y,Z);
+
+figure
+trisurf(K,X,Y,Z,'FaceAlpha',0.3)
+xlabel('log10(\omega_n)')
+ylabel('xdot')
+zlabel('Loss')
 
 
+% j = 7;
+% 
+% [Ts, OR] = meshgrid(switching_time, open_ratio_array);
+% Loss_slice = squeeze(Loss(:,j,:));
+% 
+% figure
+% surf(Ts, OR, Loss_slice')
+% set(gca,'XScale','log','YScale','log')
+% shading interp
+% xlabel('Switching Time')
+% ylabel('Opening Ratio')
+% zlabel('Loss')
+% colorbar
+% view(135,30)
+
+
+% Loss2 = squeeze(Loss(:,1,:));
+% [OR, Tsw] = meshgrid(open_ratio_array(1:3), switching_time*1e3);
+% 
 % figure;
-% plot(switching_time*1e3, squeeze(Loss), 'o-');
+% surf(OR, Tsw, Loss2);
+% xlabel('Opening ratio');
+% ylabel('Switching time (s)');
+% zlabel('Loss');
+% set(gca,'YScale','log'); % switching_time is also log-spaced here
+% % set(gca,'Ydir', 'reverse')
+% grid on;
+
+% open_area_ratio_ind = 4;
+% 
+% figure;
+% plot(switching_time*1e3, squeeze(Loss(:,:,open_area_ratio_ind)), 'o-', LineWidth=3);
 % xlabel('Switching Time (ms)')
 % ylabel('Loss (J)')
 % grid on
-% title('Throttling Losses vs. Switching Time')
+% str = ['Throttling Losses vs. Switching Time ', 'A_{valve(max)} = ' ...
+%         , num2str(open_ratio_array(open_area_ratio_ind)*param.max_Avt)];
+% title(str)
 % 
 % set(gca,'FontSize',16,'FontWeight','bold','LineWidth',1.5)
 % 
-% labels = arrayfun(@(v) sprintf('$%d$', v), ...
-%                   open_ratio_array, 'UniformOutput', false);
+% labels = arrayfun(@(v) sprintf('$%.3f$', v), ...
+%                   xdot, 'UniformOutput', false);
 % 
-% lgd = legend(labels,'Interpreter','latex', 'Location','northwest');
+% lgd = legend(labels,'Interpreter','latex','Location','northwest');
 % lgd.FontWeight = 'bold';
-% lgd.Title.String = '$Multiple~of~max~valve~opening:$';
+% lgd.Title.String = '$Speed~of~the~piston:$';
 % lgd.Title.Interpreter = 'latex';
 % lgd.Title.FontWeight = 'bold';
