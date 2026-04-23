eb21cdf1c20771382d06d4853b8a9826c69b29a6
mahmud10cool <mutashi@gmail.com>
2026-04-23 01:15:39 -0500

    More minor changes to the plot code

--- MULTIPHYSICS SIMULATION/plots_for_journal_paper.m
+++ MULTIPHYSICS SIMULATION/plots_for_journal_paper.m
@@ -3,7 +3,7 @@ clc; clear; close all;
 T_sim = 1;
 
 soft_sim = sim("soft_switch_new_simulation.slx");
-
+t_soft = soft_sim.tout;
 input_energy = soft_sim.Input_Energy.Data;
 output_energy = soft_sim.Output_Energy.Data;
 pump_loss = soft_sim.Pump_Losses.Data;
@@ -11,9 +11,21 @@ regen = soft_sim.Regen.Data;
 elec_loss = soft_sim.Elec_Losses.Data;
 throttling_loss = soft_sim.Throttling_Loss.Data;
 work = soft_sim.Work.Data;
+prv_open = soft_sim.PRV_Opening.Data;
+ssv_open = soft_sim.SSV_Opening.Data;
 
 normal_sim = sim("no_soft_switch_sim.slx");
+t_norm = normal_sim.tout;
 input_energy_norm = normal_sim.Input_Energy.Data;
 output_energy_norm = normal_sim.Output_Energy.Data;
 work_norm = normal_sim.Work.Data;
 throttling_loss_norm = normal_sim.Throttling_Loss.Data;
+prv_open_norm = normal_sim.PRV_Opening.Data;
+
+%% Plots
+figure(1)
+subplot(4,4,1)
+plot(t_soft, input_energy, t_norm, input_energy_norm, LineWidth=2)
+xlabel('Time (s)')
+ylabel('Input Energy (J)')
+grid on
